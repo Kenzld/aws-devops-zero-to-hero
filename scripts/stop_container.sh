c9aed2d5e78477b8cc8a1462755c6c1879935247
Kenzld <kenzmazin67@gmail.com>
2025-08-19 00:34:11 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Find container ID running on port 5000
-cid=$(docker ps | grep "5000" | awk '{print $1}')
+# Find container ID (running OR stopped) using port 5000
+cid=$(docker ps -a | grep "5000" | awk '{print $1}')
 
-# If container exists, stop it
 if [ -n "$cid" ]; then
-  echo "Stopping container $cid"
-  docker stop $cid
+  echo "Stopping and removing container(s) on port 5000"
+  docker stop $cid 2>/dev/null
   docker rm $cid
 else
   echo "No container found on port 5000"
+fi
+
+# Also free up port 5000 if something else is using it
+pid=$(lsof -ti:5000)
+if [ -n "$pid" ]; then
+  echo "Killing process using port 5000 (PID: $pid)"
+  kill -9 $pid
 fi
