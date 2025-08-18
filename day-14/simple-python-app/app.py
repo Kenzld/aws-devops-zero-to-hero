6ee4a0f866ca79c984c1bc34b05bebcbce2aaaf4
Kenzld <kenzmazin67@gmail.com>
2025-08-18 08:36:49 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, world!'
-#check 4 triggering code build by codepipeline
+#check 5 triggering code build by codepipeline
 if __name__ == '__main__': 
     app.run() 
                                                     
