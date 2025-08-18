707595087f055bd4fdf09e9b1cad2f6bd39c1d5c
Kenzld <kenzmazin67@gmail.com>
2025-08-18 18:21:18 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,14 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+containerid=$(docker ps | awk 'NR>1 {print $1}')
+
+if [ -n "$containerid" ]; then
+  echo "Stopping container(s): $containerid"
+  docker stop $containerid
+
+  echo "Removing container(s): $containerid"
+  docker rm $containerid
+else
+  echo "No running containers found."
+fi
